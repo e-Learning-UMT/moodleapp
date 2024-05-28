e-Learning-UMT/moodleapp

commit 91c56256ed40caa6479acd9c2233410d4a25e58f

MOBILE-4470 courses-storage: Update site size after delete data

src/addons/storagemanager/pages/courses-storage/courses-storage.ts
@@ -87,9 +87,7 @@ export class AddonStorageManagerCoursesStoragePage implements OnInit, OnDestroy 
             }
         }
 
-        this.spaceUsage = await CoreSettingsHelper.getSiteSpaceUsage(this.siteId);
-
-        this.setDownloadedCourses(downloadedCourses);
+        await this.setDownloadedCourses(downloadedCourses);
 
         this.loaded = true;
     }
@@ -126,7 +124,7 @@ export class AddonStorageManagerCoursesStoragePage implements OnInit, OnDestroy 
         try {
             await Promise.all(deletedCourseIds.map((courseId) => CoreCourseHelper.deleteCourseFiles(courseId)));
 
-            this.setDownloadedCourses(this.downloadedCourses.filter((course) => !deletedCourseIds.includes(course.id)));
+            await this.setDownloadedCourses(this.downloadedCourses.filter((course) => !deletedCourseIds.includes(course.id)));
         } catch (error) {
             CoreDomUtils.showErrorModalDefault(error, Translate.instant('core.errordeletefile'));
         } finally {
@@ -162,7 +160,7 @@ export class AddonStorageManagerCoursesStoragePage implements OnInit, OnDestroy 
         try {
             await CoreCourseHelper.deleteCourseFiles(course.id);
 
-            this.setDownloadedCourses(CoreArray.withoutItem(this.downloadedCourses, course));
+            await this.setDownloadedCourses(CoreArray.withoutItem(this.downloadedCourses, course));
         } catch (error) {
             CoreDomUtils.showErrorModalDefault(error, Translate.instant('core.errordeletefile'));
         } finally {
@@ -177,7 +175,7 @@ export class AddonStorageManagerCoursesStoragePage implements OnInit, OnDestroy 
      */
     private async onCourseUpdated(courseId: number, status: DownloadStatus): Promise<void> {
         if (courseId == CoreCourseProvider.ALL_COURSES_CLEARED) {
-            this.setDownloadedCourses([]);
+            await this.setDownloadedCourses([]);
 
             return;
         }
@@ -191,7 +189,7 @@ export class AddonStorageManagerCoursesStoragePage implements OnInit, OnDestroy 
         course.isDownloading = status === DownloadStatus.DOWNLOADING;
         course.totalSize = await this.calculateDownloadedCourseSize(course.id);
 
-        this.setDownloadedCourses(this.downloadedCourses);
+        await this.setDownloadedCourses(this.downloadedCourses);
     }
 
     /**
@@ -199,7 +197,10 @@ export class AddonStorageManagerCoursesStoragePage implements OnInit, OnDestroy 
      *
      * @param courses Courses info.
      */
-    private setDownloadedCourses(courses: DownloadedCourse[]): void {
+    private async setDownloadedCourses(courses: DownloadedCourse[]): Promise<void> {
+        // Downloaded courses changed, update site usage too.
+        this.spaceUsage = await CoreSettingsHelper.getSiteSpaceUsage(this.siteId);
+
         this.downloadedCourses = courses.sort((a, b) => b.totalSize - a.totalSize);
         this.completelyDownloadedCourses = courses.filter((course) => !course.isDownloading);
         this.totalSize = this.downloadedCourses.reduce((totalSize, course) => totalSize + course.totalSize, 0);

commit 0e42c1ee155c83cc4a301df2e9f346556a48e28f

MOBILE-4470 storagemanager: Fix removing courses
Fixes a regression caused by https://github.com/moodlehq/moodleapp/commit/91c56256ed40caa6479acd9c2233410d4a25e58f

src/addons/storagemanager/pages/courses-storage/courses-storage.ts
@@ -14,6 +14,7 @@
 
 import { DownloadStatus } from '@/core/constants';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CoreQueueRunner } from '@classes/queue-runner';
 import { CoreCourse, CoreCourseProvider } from '@features/course/services/course';
 import { CoreCourseHelper } from '@features/course/services/course-helper';
 import { CoreCourseModulePrefetchDelegate } from '@features/course/services/module-prefetch-delegate';
@@ -49,6 +50,8 @@ export class AddonStorageManagerCoursesStoragePage implements OnInit, OnDestroy 
 
     courseStatusObserver?: CoreEventObserver;
     siteId: string;
+
+    private downloadedCoursesQueue = new CoreQueueRunner();
 
     constructor() {
         this.siteId = CoreSites.getCurrentSiteId();
@@ -87,7 +90,7 @@ export class AddonStorageManagerCoursesStoragePage implements OnInit, OnDestroy 
             }
         }
 
-        await this.setDownloadedCourses(downloadedCourses);
+        await this.downloadedCoursesQueue.run(() => this.setDownloadedCourses(downloadedCourses));
 
         this.loaded = true;
     }
@@ -124,7 +127,9 @@ export class AddonStorageManagerCoursesStoragePage implements OnInit, OnDestroy 
         try {
             await Promise.all(deletedCourseIds.map((courseId) => CoreCourseHelper.deleteCourseFiles(courseId)));
 
-            await this.setDownloadedCourses(this.downloadedCourses.filter((course) => !deletedCourseIds.includes(course.id)));
+            await this.downloadedCoursesQueue.run(async () => {
+                await this.setDownloadedCourses(this.downloadedCourses.filter((course) => !deletedCourseIds.includes(course.id)));
+            });
         } catch (error) {
             CoreDomUtils.showErrorModalDefault(error, Translate.instant('core.errordeletefile'));
         } finally {
@@ -160,7 +165,9 @@ export class AddonStorageManagerCoursesStoragePage implements OnInit, OnDestroy 
         try {
             await CoreCourseHelper.deleteCourseFiles(course.id);
 
-            await this.setDownloadedCourses(CoreArray.withoutItem(this.downloadedCourses, course));
+            await this.downloadedCoursesQueue.run(async () => {
+                await this.setDownloadedCourses(CoreArray.withoutItem(this.downloadedCourses, course));
+            });
         } catch (error) {
             CoreDomUtils.showErrorModalDefault(error, Translate.instant('core.errordeletefile'));
         } finally {
@@ -175,7 +182,7 @@ export class AddonStorageManagerCoursesStoragePage implements OnInit, OnDestroy 
      */
     private async onCourseUpdated(courseId: number, status: DownloadStatus): Promise<void> {
         if (courseId == CoreCourseProvider.ALL_COURSES_CLEARED) {
-            await this.setDownloadedCourses([]);
+            await this.downloadedCoursesQueue.run(() => this.setDownloadedCourses([]));
 
             return;
         }
@@ -189,7 +196,7 @@ export class AddonStorageManagerCoursesStoragePage implements OnInit, OnDestroy 
         course.isDownloading = status === DownloadStatus.DOWNLOADING;
         course.totalSize = await this.calculateDownloadedCourseSize(course.id);
 
-        await this.setDownloadedCourses(this.downloadedCourses);
+        await this.downloadedCoursesQueue.run(() => this.setDownloadedCourses(this.downloadedCourses));
     }
 
     /**

src/core/classes/queue-runner.ts
@@ -121,8 +121,17 @@ export class CoreQueueRunner {
      * @param options Options.
      * @returns Promise resolved when the function has been executed.
      */
-    run<T>(id: string, fn: CoreQueueRunnerFunction<T>, options?: CoreQueueRunnerAddOptions): Promise<T> {
-        options = options || {};
+    run<T>(fn: CoreQueueRunnerFunction<T>, options?: CoreQueueRunnerAddOptions): Promise<T>;
+    run<T>(id: string, fn: CoreQueueRunnerFunction<T>, options?: CoreQueueRunnerAddOptions): Promise<T>;
+    run<T>(
+        idOrFn: string | CoreQueueRunnerFunction<T>,
+        fnOrOptions?: CoreQueueRunnerFunction<T> | CoreQueueRunnerAddOptions,
+        options: CoreQueueRunnerAddOptions = {},
+    ): Promise<T> {
+        let id = typeof idOrFn === 'string' ? idOrFn : this.getUniqueId('anonymous');
+        const fn = typeof idOrFn === 'function' ? idOrFn : fnOrOptions as CoreQueueRunnerFunction<T>;
+
+        options = typeof fnOrOptions === 'object' ? fnOrOptions : options;
 
         if (id in this.queue) {
             if (!options.allowRepeated) {

src/core/classes/tests/queue-runner.test.ts
@@ -1,0 +1,38 @@
+// (C) Copyright 2015 Moodle Pty Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { CoreQueueRunner } from '@classes/queue-runner';
+import { CoreUtils } from '@services/utils/utils';
+
+describe('CoreQueueRunner', () => {
+
+    it('Locks threads launched synchronously', async () => {
+        // Arrange
+        const concurrency = 100;
+        const range = Array.from({ length: concurrency }, (_, item) => item);
+        const lock = new CoreQueueRunner();
+        const items: string[] = [];
+
+        // Act
+        await Promise.all(range.map((i) => lock.run(async () => {
+            await CoreUtils.wait(Math.floor(Math.random() * 10));
+
+            items.push(`Item #${i}`);
+        })));
+
+        // Assert
+        expect(items).toEqual(range.map(i => `Item #${i}`));
+    });
+
+});
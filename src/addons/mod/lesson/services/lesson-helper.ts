e-Learning-UMT/moodleapp

commit 2449aca7813a73b31e2527271a53dcc7ca064eaf

MOBILE-3947 ng: Fix build errors
Target in app tsconfig is set to es2022 by the Angular CLI, so we must
set it as well to be consistent in the rest of the tooling. Angular
compilation later uses browserslist for further transpilations.

Target in unit tests is kept at es2016 because of a known bug in
Angular: https://github.com/angular/angular/issues/31730

src/addons/mod/lesson/services/lesson-helper.ts
@@ -270,7 +270,7 @@ export class AddonModLessonHelperProvider {
             if (option.checked || multiChoiceQuestion.multi) {
                 // Add the control.
                 const value = multiChoiceQuestion.multi ?
-                    { value: option.checked, disabled: option.disabled } : option.value;
+                    { value: option.checked, disabled: option.disabled } : option.checked;
                 questionForm.addControl(option.name, this.formBuilder.control(value));
                 controlAdded = true;
             }

src/addons/mod/lesson/pages/user-retake/user-retake.ts
@@ -218,7 +218,7 @@ export class AddonModLessonUserRetakePage implements OnInit {
      * @returns Formatted data.
      */
     protected formatRetake(retakeData: AddonModLessonGetUserAttemptWSResponse): RetakeToDisplay {
-        const formattedData = <RetakeToDisplay> retakeData;
+        const formattedData = retakeData;
 
         if (formattedData.userstats.gradeinfo) {
             // Completed.
@@ -229,19 +229,23 @@ export class AddonModLessonUserRetakePage implements OnInit {
         // Format pages data.
         formattedData.answerpages.forEach((page) => {
             if (AddonModLesson.answerPageIsContent(page)) {
-                page.isContent = true;
+                const contentPage = page as AnswerPage;
 
-                if (page.answerdata?.answers) {
-                    page.answerdata.answers.forEach((answer) => {
+                contentPage.isContent = true;
+
+                if (contentPage.answerdata?.answers) {
+                    contentPage.answerdata.answers.forEach((answer) => {
                         // Content pages only have 1 valid field in the answer array.
                         answer[0] = AddonModLessonHelper.getContentPageAnswerDataFromHtml(answer[0]);
                     });
                 }
             } else if (AddonModLesson.answerPageIsQuestion(page)) {
-                page.isQuestion = true;
+                const questionPage = page as AnswerPage;
 
-                if (page.answerdata?.answers) {
-                    page.answerdata.answers.forEach((answer) => {
+                questionPage.isQuestion = true;
+
+                if (questionPage.answerdata?.answers) {
+                    questionPage.answerdata.answers.forEach((answer) => {
                         // Only the first field of the answer array requires to be parsed.
                         answer[0] = AddonModLessonHelper.getQuestionPageAnswerDataFromHtml(answer[0]);
                     });

src/addons/mod/scorm/services/scorm.ts
@@ -337,7 +337,7 @@ export class AddonModScormProvider {
 
                 const re = /^(\d+)\*\{(.+)\}$/; // Sets like 3*{S34, S36, S37, S39}.
                 const reOther = /^(.+)(=|<>)(.+)$/; // Other symbols.
-                let matches = element.match(re);
+                const matches = element.match(re);
 
                 if (matches) {
                     const repeat = Number(matches[1]);
@@ -363,18 +363,18 @@ export class AddonModScormProvider {
                     element = '!';
                 } else if (reOther.test(element)) {
                     // Other symbols = | <> .
-                    matches = element.match(reOther) ?? [];
-                    element = matches[1]?.trim();
+                    const otherMatches = element.match(reOther) ?? [];
+                    element = otherMatches[1]?.trim();
 
                     if (trackData[element] !== undefined) {
-                        let value = matches[3].trim().replace(/('|")/gi, '');
+                        let value = otherMatches[3].trim().replace(/('|")/gi, '');
                         let oper: string;
 
                         if (STATUSES[value] !== undefined) {
                             value = STATUSES[value];
                         }
 
-                        if (matches[2] == '<>') {
+                        if (otherMatches[2] == '<>') {
                             oper = '!=';
                         } else {
                             oper = '==';

src/app/app-routing.module.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { InjectionToken, Injector, ModuleWithProviders, NgModule } from '@angular/core';
+import { InjectionToken, Injector, ModuleWithProviders, NgModule, Type } from '@angular/core';
 import {
     PreloadAllModules,
     RouterModule,
@@ -96,6 +96,12 @@ function buildConditionalUrlMatcher(pathOrMatcher: string | UrlMatcher, conditio
         return { consumed: segments.slice(0, parts.length), posParams };
     };
 }
+
+/**
+ * Type to declare lazy route modules.
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type LazyRoutesModule = Type<any>;
 
 /**
  * Build url matcher using a regular expression.

src/core/classes/application-init-status.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { ApplicationInitStatus, APP_INITIALIZER, Injectable, Injector } from '@angular/core';
+import { ApplicationInitStatus, Injectable, Injector } from '@angular/core';
 import { setSingletonsInjector } from '@singletons';
 
 @Injectable()
@@ -21,7 +21,7 @@ export class CoreApplicationInitStatus extends ApplicationInitStatus {
     constructor(injector: Injector) {
         setSingletonsInjector(injector);
 
-        super(injector.get(APP_INITIALIZER, []));
+        super();
     }
 
     whenDone(callback: () => unknown): void {

src/core/features/course/services/course-helper.ts
@@ -75,6 +75,7 @@ import { CoreRemindersPushNotificationData } from '@features/reminders/services/
 import { CoreLocalNotifications } from '@services/local-notifications';
 import { CoreEnrol } from '@features/enrol/services/enrol';
 import { CoreEnrolAction, CoreEnrolDelegate } from '@features/enrol/services/enrol-delegate';
+import { LazyRoutesModule } from '@/app/app-routing.module';
 
 /**
  * Prefetch info of a module.
@@ -1990,7 +1991,7 @@ export class CoreCourseHelperProvider {
      *
      * @returns Course summary page module.
      */
-    async getCourseSummaryRouteModule(): Promise<unknown> {
+    async getCourseSummaryRouteModule(): Promise<LazyRoutesModule> {
         return import('../course-summary-lazy.module').then(m => m.CoreCourseSummaryLazyModule);
     }
 

src/core/features/courses/services/courses-helper.ts
@@ -30,6 +30,7 @@ import { of, firstValueFrom } from 'rxjs';
 import { zipIncludingComplete } from '@/core/utils/rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { chainRequests, WSObservable } from '@classes/sites/authenticated-site';
+import { LazyRoutesModule } from '@/app/app-routing.module';
 
 // Id for a course item representing all courses (for example, for course filters).
 export const ALL_COURSES_ID = -1;
@@ -432,7 +433,7 @@ export class CoreCoursesHelperProvider {
      *
      * @returns My courses page module.
      */
-    async getMyRouteModule(): Promise<unknown> {
+    async getMyRouteModule(): Promise<LazyRoutesModule> {
         return import('../courses-my-lazy.module').then(m => m.CoreCoursesMyLazyModule);
     }
 

src/core/features/emulator/services/file.ts
@@ -12,8 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* eslint-disable deprecation/deprecation */
+
 import { Injectable } from '@angular/core';
-import { File, Entry, DirectoryEntry, FileEntry, IWriteOptions, RemoveResult } from '@awesome-cordova-plugins/file/ngx';
+import {
+    File,
+    Entry,
+    FileEntry,
+    FileSystem,
+    IWriteOptions,
+    RemoveResult,
+    DirectoryEntry,
+    DirectoryReader,
+} from '@awesome-cordova-plugins/file/ngx';
 import { CorePath } from '@singletons/path';
 
 /**
@@ -39,6 +50,82 @@ class FileError {
     constructor(
         public code: number,
     ) { }
+
+}
+
+/**
+ * Native APIs used in webkit window.
+ */
+interface WebkitWindow {
+
+    /**
+     * @deprecated
+     * @see https://www.w3.org/TR/2012/WD-file-system-api-20120417/
+     */
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    LocalFileSystem: {
+        readonly TEMPORARY: number;
+        readonly PERSISTENT: number;
+    };
+
+    /**
+     * @deprecated
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/Window/requestFileSystem
+     */
+    requestFileSystem(
+        type: LocalFileSystem,
+        size: number,
+        successCallback: (fileSystem: FileSystem) => void,
+        errorCallback?: (fileError: FileError) => void,
+    ): void;
+
+    /**
+     * @deprecated
+     */
+    webkitRequestFileSystem(
+        type: LocalFileSystem,
+        size: number,
+        successCallback: (fileSystem: FileSystem) => void,
+        errorCallback?: (fileError: FileError) => void,
+    ): void;
+
+    /**
+     * @deprecated
+     * @see https://www.w3.org/TR/2012/WD-file-system-api-20120417/
+     */
+    resolveLocalFileSystemURL(
+        url: string,
+        successCallback: (entry: Entry) => void,
+        errorCallback?: (fileError: FileError) => void,
+    ): void;
+
+    /**
+     * @deprecated
+     */
+    webkitResolveLocalFileSystemURL(
+        url: string,
+        successCallback: (entry: Entry) => void,
+        errorCallback?: (fileError: FileError) => void,
+    ): void;
+
+}
+
+/**
+ * Native APIs used in webkit navigator.
+ */
+interface WebkitNavigator {
+
+    /**
+     * @deprecated
+     * @see https://developer.chrome.com/docs/apps/offline_storage/
+     */
+    webkitPersistentStorage: {
+        requestQuota(
+            newQuotaInBytes: number,
+            successCallback?: (bytesGranted: number) => void,
+            errorCallback?: (error: Error) => void,
+        ): void;
+    };
 
 }
 
@@ -285,39 +372,40 @@ export class FileMock extends File {
      */
     async getFreeDiskSpace(): Promise<number> {
         // Request a file system instance with a minimum size until we get an error.
-        if (window.requestFileSystem) {
-            let iterations = 0;
-            let maxIterations = 50;
-            const calculateByRequest = (size: number, ratio: number): Promise<number> =>
-                new Promise((resolve): void => {
-                    window.requestFileSystem(LocalFileSystem.PERSISTENT, size, () => {
-                        iterations++;
-                        if (iterations > maxIterations) {
-                            resolve(size);
+        const window = this.getEmulatorWindow();
 
-                            return;
-                        }
-                        // eslint-disable-next-line promise/catch-or-return
-                        calculateByRequest(size * ratio, ratio).then(resolve);
-                    }, () => {
-                        resolve(size / ratio);
-                    });
-                });
-
-            // General calculation, base 1MB and increasing factor 1.3.
-            let size = await calculateByRequest(1048576, 1.3);
-
-            // More accurate. Factor is 1.1.
-            iterations = 0;
-            maxIterations = 10;
-
-            size = await calculateByRequest(size, 1.1);
-
-            return size / 1024; // Return size in KB.
-
-        } else {
+        if (!window.requestFileSystem) {
             throw new Error('File system not available.');
         }
+
+        let iterations = 0;
+        let maxIterations = 50;
+        const calculateByRequest = (size: number, ratio: number): Promise<number> =>
+            new Promise((resolve): void => {
+                window.requestFileSystem(LocalFileSystem.PERSISTENT, size, () => {
+                    iterations++;
+                    if (iterations > maxIterations) {
+                        resolve(size);
+
+                        return;
+                    }
+                    // eslint-disable-next-line promise/catch-or-return
+                    calculateByRequest(size * ratio, ratio).then(resolve);
+                }, () => {
+                    resolve(size / ratio);
+                });
+            });
+
+        // General calculation, base 1MB and increasing factor 1.3.
+        let size = await calculateByRequest(1048576, 1.3);
+
+        // More accurate. Factor is 1.1.
+        iterations = 0;
+        maxIterations = 10;
+
+        size = await calculateByRequest(size, 1.1);
+
+        return size / 1024; // Return size in KB.
     }
 
     /**
@@ -342,24 +430,23 @@ export class FileMock extends File {
      */
     load(): Promise<string> {
         return new Promise((resolve, reject): void => {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            const win = <any> window; // Convert to <any> to be able to use non-standard properties.
+            const window = this.getEmulatorWindow();
 
-            if (win.requestFileSystem === undefined) {
-                win.requestFileSystem = win.webkitRequestFileSystem;
+            if (window.requestFileSystem === undefined) {
+                window.requestFileSystem = window.webkitRequestFileSystem;
             }
-            if (win.resolveLocalFileSystemURL === undefined) {
-                win.resolveLocalFileSystemURL = win.webkitResolveLocalFileSystemURL;
+            if (window.resolveLocalFileSystemURL === undefined) {
+                window.resolveLocalFileSystemURL = window.webkitResolveLocalFileSystemURL;
             }
-            win.LocalFileSystem = {
+            window.LocalFileSystem = {
+                TEMPORARY: 0, // eslint-disable-line @typescript-eslint/naming-convention
                 PERSISTENT: 1, // eslint-disable-line @typescript-eslint/naming-convention
             };
 
             // Request a quota to use. Request 500MB.
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            (<any> navigator).webkitPersistentStorage.requestQuota(500 * 1024 * 1024, (granted) => {
-                window.requestFileSystem(LocalFileSystem.PERSISTENT, granted, (entry) => {
-                    resolve(entry.root.toURL());
+            this.getEmulatorNavigator().webkitPersistentStorage.requestQuota(500 * 1024 * 1024, (granted) => {
+                window.requestFileSystem(LocalFileSystem.PERSISTENT, granted, (fileSystem: FileSystem) => {
+                    resolve(fileSystem.root.toURL());
                 }, reject);
             }, reject);
         });
@@ -642,7 +729,7 @@ export class FileMock extends File {
     resolveLocalFilesystemUrl(fileUrl: string): Promise<Entry> {
         return new Promise<Entry>((resolve, reject): void => {
             try {
-                window.resolveLocalFileSystemURL(fileUrl, (entry: Entry) => {
+                this.getEmulatorWindow().resolveLocalFileSystemURL(fileUrl, (entry: Entry) => {
                     resolve(entry);
                 }, (error: FileError) => {
                     this.fillErrorMessageMock(error);
@@ -797,6 +884,24 @@ export class FileMock extends File {
             };
             writeNextChunk();
         });
+    }
+
+    /**
+     * Get emulator window.
+     *
+     * @returns Emulator window.
+     */
+    private getEmulatorWindow(): WebkitWindow {
+        return window as unknown as WebkitWindow;
+    }
+
+    /**
+     * Get emulator navigator.
+     *
+     * @returns Emulator navigator.
+     */
+    private getEmulatorNavigator(): WebkitNavigator {
+        return navigator as unknown as WebkitNavigator;
     }
 
 }

commit 699ab8cd3bc2235551a968edd42ae3cc1258362a

MOBILE-4470 lesson: Fix multichoice option not selected in review
Fixes a regression caused by 2449aca

src/addons/mod/lesson/services/lesson-helper.ts
@@ -269,9 +269,14 @@ export class AddonModLessonHelperProvider {
 
             if (option.checked || multiChoiceQuestion.multi) {
                 // Add the control.
-                const value = multiChoiceQuestion.multi ?
-                    { value: option.checked, disabled: option.disabled } : option.checked;
-                questionForm.addControl(option.name, this.formBuilder.control(value));
+                if (multiChoiceQuestion.multi) {
+                    questionForm.addControl(
+                        option.name,
+                        this.formBuilder.control({ value: option.checked, disabled: option.disabled }),
+                    );
+                } else {
+                    questionForm.addControl(option.name, this.formBuilder.control(option.value));
+                }
                 controlAdded = true;
             }
 